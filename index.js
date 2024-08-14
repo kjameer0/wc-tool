f659e3a3d56345311adb590284781186681f5f03
Khalid Jameer <41968688+kjameer0@users.noreply.github.com>
2024-08-14 12:05:43 -0400

    feat: read arguments from terminal command

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("this is my ccwc")
+const { argv } = require("node:process");
+
+// print process.argv
+
+function parseTerminalArguments(args) {
+  const argRecord = {};
+  //check that there is at least one parameter provided and it is a path
+  if (args.length === 0 || !args.at(-1).includes(".")) {
+    throw new Error("Please supply a valid path");
+  }
+  argRecord.path = args.pop();
+  //only take first option
+  if (args.length > 0) {
+    argRecord.option = args[0];
+  }
+  return argRecord;
+}
+console.log("this is my ccwc");
+// getting a file path
+// reading parameters from terminal command
+// process the file
+//read a file
+function main() {
+  const { path, option } = parseTerminalArguments(argv.slice(2));
+  console.log("here are the options ", path, " ", option);
+}
+main();
